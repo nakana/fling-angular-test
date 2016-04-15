d229557ed0aaae00208108bd6d5e2e2b5a61d922
nakana <t-nakanishi@esm.co.jp>
2016-04-15 17:38:16 +0900

    とりあえずサーバでステータス500を返す

--- src/test/dataServiceSpec.js
+++ src/test/dataServiceSpec.js
@@ -20,5 +20,23 @@ describe("サービステスト", function(){
     $httpBackend.flush();
     expect(result).toEqual('mock-data-a');
   }));
+
+  it("想定データを受けとれないテスト", inject(function(dataService){
+    $httpBackend.expect('GET', '/test/a').respond(500, '', {Expires: 0});
+    var result;
+    dataService.getData('a').get(function(data){
+      result = data.value;
+    }, function(data){
+      console.log(data);
+      console.log(data.status);
+      console.log(data.headers());
+      console.log(data.headers('expires'));
+      result = 'error';
+    });
+    $httpBackend.flush();
+
+    expect(result).toEqual('error');
+  }));
 });
+  
 
